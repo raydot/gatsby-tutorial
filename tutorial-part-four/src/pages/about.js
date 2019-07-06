b8c610d60a584d3c2452d043c1e8226cf980217e
Dave Kanter <moatgoat@hotmail.com>
2019-07-05 22:03:02 -0700

    Started tutorial part 4

---
+++ tutorial-part-four/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+        <h1>About Pandas Eating Lots</h1>
+            <p>
+                This is a site that will blow your mind with all of its Panda pandamonium.  You won't believe your eyes, step right this way, prepare to be amazed!
+            </p>
+    </Layout>
+) 
